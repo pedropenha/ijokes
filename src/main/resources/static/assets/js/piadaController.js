a4b07573bf3aa909f910e7ff0b36eb38a0d2453e
gustavo ferruci <guferruci@gmail.com>
2022-06-05 15:00:30 -0300

    modificado pagina de insere piadas e rota de piadas

--- src/main/resources/static/assets/js/piadaController.js
+++ src/main/resources/static/assets/js/piadaController.js
@@ -5,30 +5,32 @@ formulario.addEventListener("submit", (e) => {
     e.preventDefault()
 
     let data
-    let categoria
-
-    fetch("/apis/listar-with-filter?filtro="+ (new URL(document.location)).searchParams.get("filtro"), {
+    let cat
+    // selecionar para enviar
+    fetch("/apis/listar-id-cat?id="+document.getElementById("categoria").value ,{
         method: 'GET',
     }).then((response) => {
         return response.text()
     }).then((text) => {
-        categoria = JSON.parse(text)
+        cat = JSON.parse(text)
         console.log(JSON.parse(text))
     }).catch((error) => {
         console.log(error)
     })
 
+    let tit = document.getElementById("titulo").value
+    let text = document.getElementById("texto").value
+    let key = document.getElementById("keywords").value
+
     data = {
-        titulo: "",
-        texto: "",
-        keywords: "",
-        ranking: "",
-        categoria: {
-            id: "",
-            nome: ""
-        }
+        titulo: tit,
+        texto: text,
+        keywords: key,
+        ranking: 0,
+        categoria: cat
     }
 
+    //cadastrar piada
     fetch(URL_TO_FETCH, {
         method: 'POST',
         body: JSON.stringify(data),
@@ -37,7 +39,25 @@ formulario.addEventListener("submit", (e) => {
         return response.text()
     }).then((text) => {
         console.log(text)
+        window.location.href = "../../index.html"
     }).catch((error) => {
         console.log(error)
     })
 })
+
+let categoria = document.getElementById("categoria")
+    // serve para exibir e selecionar
+    fetch("/apis/listar-todas-categorias", { 
+        method: 'GET',
+    }).then((response) => {
+        return response.text()
+    }).then((text) => {
+        categoria = JSON.parse(text)
+        console.log(JSON.parse(text))
+        let aux = JSON.parse(text) 
+        for (let index = 0; index < aux.length; index++) {
+            categoria.innerHTML+="<option value="+aux[index].id+">"+aux[index].nome+"</option>"
+        }
+    }).catch((error) => {
+        console.log(error)
+    })
